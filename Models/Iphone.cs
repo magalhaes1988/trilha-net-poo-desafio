b0f60f8e911152da8f221a48408b818193ff1eb0
Marcelo Bevilaqua Magalhaes <magalhaes1988@gmail.com>
2022-08-24 12:17:56 -0300

    REFACTOR

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -4,10 +4,10 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
+    
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
         
-        public Iphone(string numero, string modelo, string imei, int memoria)
-        {   
-            this.Numero = numero;
         }
 
         // TODO: Sobrescrever o método "InstalarAplicativo"
