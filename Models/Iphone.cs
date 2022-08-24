0d0787903c53c967f0c0d2722dc1061acce07a31
Marcelo Bevilaqua Magalhaes <magalhaes1988@gmail.com>
2022-08-24 12:16:39 -0300

    FEAT: New Functions

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
+
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        
+        public Iphone(string numero, string modelo, string imei, int memoria)
+        {   
+            this.Numero = numero;
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o Aplicativo {nomeApp}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "11987654321", modelo: "N95", imei: "1234567891022", memoria: 64);
+Smartphone iphone = new Iphone(numero: "18965473215", modelo: "iPhone 13", imei:"98765432101236", memoria:512);
+
+Console.WriteLine("Smartphone Nokia");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+Console.WriteLine("Smartphone iPhone");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Canvas");
